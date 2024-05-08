6048afc0567513b4b52caab3687f80b63ec5993c
yuzusiooo <70190614+yuzusiooo@users.noreply.github.com>
2024-05-08 23:26:46 +0900

    main menu updates
    new background image and buttons that react on hover

--- Assets/Scripts/MainMenu/MainMenuUI/MainMenuUI.cs
+++ Assets/Scripts/MainMenu/MainMenuUI/MainMenuUI.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuUI : MonoBehaviour
 {
 
     public SceneAsset stageSelectScene;
 
+    [Header("Menu Selcetion Image")]
+    public Image SelectionImage;
+    public Sprite StartSelectionIcon;
+    public Sprite QuitSelectionIcon;
     // call the scene loader and change to the stage select scene
     public void StageSelect()
     {
@@ -18,5 +23,22 @@ public class MainMenuUI : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
+    }
+
+    // when the mouse cursor hovers over the button, change the selection image to the appropriate button icon
+    public void MenuButtonHover(string buttonName)
+    {
+        // dictionary to match name and sprite
+        Dictionary<string, Sprite> selectionIcon = new Dictionary<string, Sprite>
+        {
+            {"start", StartSelectionIcon },
+            {"quit", QuitSelectionIcon }
+        };
+
+        if (selectionIcon[buttonName])
+        {
+            SelectionImage.sprite = selectionIcon[buttonName];
+            SelectionImage.color = Color.white;
+        }
     }
 }
